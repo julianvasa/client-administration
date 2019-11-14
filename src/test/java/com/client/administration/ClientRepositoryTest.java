dee645487ce70d4bb8b0a62caf4b1b8c5f2c96f8
julianvasa <vasa.julian@gmail.com>
2019-11-14 22:04:18 +0100

    Client repository integration test

---
+++ src/test/java/com/client/administration/ClientRepositoryTest.java
@@ -0,0 +1,39 @@
+package com.client.administration;
+
+import com.client.administration.model.Address;
+import com.client.administration.model.Client;
+import com.client.administration.repository.ClientRepository;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.assertNotNull;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+public class ClientRepositoryTest {
+
+    @Autowired
+    private ClientRepository clientRepository;
+
+    @Autowired
+    private TestEntityManager testEntityManager;
+
+    @Test
+    public void persistUser(){
+        Address ADDRESS = Address.builder().street("Street").city("City").country("Country").zip(1000).build();
+
+        Client client = new Client();
+        client.setFirstName("FirstName");
+        client.setLastName("LastName");
+        client.setEmail("user@test.com");
+        client.setAddress(ADDRESS);
+
+        testEntityManager.persist(client);
+        assertNotNull(clientRepository.findById(1L));
+    }
+
+}
